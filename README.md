b72e6ada3e2b0ec916e39e3defb96beda57da31d
Margaux Gérard <margauxgerard@MacBook-Pro-de-Margaux.local>
2022-11-07 23:34:34 +0100

    Deployment terraform infra

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# generate_code
+# Deploying a Web application on GCP
+
+The goal of this personal project is to deploy a Flask Web application on GCP. 
+
+
+
--- infra/main.tf
+++ infra/main.tf
@@ -15,29 +15,29 @@ provider "google-beta" {
 
 # Enable IAM API
 resource "google_project_service" "iam" {
-  provider = google-beta
-  service = "iam.googleapis.com"
+  provider           = google-beta
+  service            = "iam.googleapis.com"
   disable_on_destroy = false
 }
 
 # Enable Artifact Registry API
 resource "google_project_service" "artifactregistry" {
-  provider = google-beta
-  service = "artifactregistry.googleapis.com"
+  provider           = google-beta
+  service            = "artifactregistry.googleapis.com"
   disable_on_destroy = false
 }
 
 # Enable Cloud Run API
 resource "google_project_service" "cloudrun" {
-  provider = google-beta
-  service = "run.googleapis.com"
+  provider           = google-beta
+  service            = "run.googleapis.com"
   disable_on_destroy = false
 }
 
 # Enable Cloud Resource Manager API
 resource "google_project_service" "resourcemanager" {
-  provider = google-beta
-  service = "cloudresourcemanager.googleapis.com"
+  provider           = google-beta
+  service            = "cloudresourcemanager.googleapis.com"
   disable_on_destroy = false
 }
 
@@ -55,33 +55,33 @@ resource "time_sleep" "wait_30_seconds" {
 
 # Create Artifact Registry Repository for Docker containers
 resource "google_artifact_registry_repository" "my_docker_repo" {
-  provider = google-beta
-  location = var.region
+  provider      = google-beta
+  location      = var.region
   repository_id = var.repository
-  description = "My docker repository"
-  format = "DOCKER"
-  depends_on = [time_sleep.wait_30_seconds]
+  description   = "My docker repository"
+  format        = "DOCKER"
+  depends_on    = [time_sleep.wait_30_seconds]
 }
 
 # Create a service account
 resource "google_service_account" "docker_pusher" {
-  provider = google-beta
+  provider     = google-beta
   account_id   = "docker-pusher"
   display_name = "Docker Container Pusher"
-  depends_on =[time_sleep.wait_30_seconds]
+  depends_on   = [time_sleep.wait_30_seconds]
 }
 
 # Give service account permission to push to the Artifact Registry Repository
 resource "google_artifact_registry_repository_iam_member" "docker_pusher_iam" {
-  provider = google-beta
-  location = google_artifact_registry_repository.my_docker_repo.location
-  repository =  google_artifact_registry_repository.my_docker_repo.repository_id
-  role   = "roles/artifactregistry.writer"
-  member = "serviceAccount:${google_service_account.docker_pusher.email}"
+  provider   = google-beta
+  location   = google_artifact_registry_repository.my_docker_repo.location
+  repository = google_artifact_registry_repository.my_docker_repo.repository_id
+  role       = "roles/artifactregistry.writer"
+  member     = "serviceAccount:${google_service_account.docker_pusher.email}"
   depends_on = [
     google_artifact_registry_repository.my_docker_repo,
     google_service_account.docker_pusher
-    ]
+  ]
 }
 
 # Deploy image to Cloud Run
@@ -91,25 +91,25 @@ resource "google_cloud_run_service" "api_test" {
   location = var.region
   template {
     spec {
-        containers {
-            image = "europe-west1-docker.pkg.dev/${var.project_id}/${var.repository}/${var.docker_image}"
-            resources {
-                limits = {
-                "memory" = "1G"
-                "cpu" = "1"
-                }
-            }
+      containers {
+        image = "europe-west1-docker.pkg.dev/${var.project_id}/${var.repository}/${var.docker_image}"
+        resources {
+          limits = {
+            "memory" = "1G"
+            "cpu"    = "1"
+          }
         }
+      }
     }
     metadata {
-        annotations = {
-            "autoscaling.knative.dev/minScale" = "0"
-            "autoscaling.knative.dev/maxScale" = "1"
-        }
+      annotations = {
+        "autoscaling.knative.dev/minScale" = "0"
+        "autoscaling.knative.dev/maxScale" = "1"
+      }
     }
   }
   traffic {
-    percent = 100
+    percent         = 100
     latest_revision = true
   }
   depends_on = [google_artifact_registry_repository_iam_member.docker_pusher_iam]
@@ -128,7 +128,7 @@ data "google_iam_policy" "noauth" {
 
 # Apply the no-authentication policy to our Cloud Run Service.
 resource "google_cloud_run_service_iam_policy" "noauth" {
-  provider = google-beta
+  provider    = google-beta
   location    = var.region
   project     = var.project_id
   service     = google_cloud_run_service.api_test.name
